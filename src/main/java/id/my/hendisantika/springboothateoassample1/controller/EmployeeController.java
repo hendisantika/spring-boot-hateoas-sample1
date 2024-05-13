c1f3b0283e68ed88896d87f7a678a7a74ae9b918
hendisantika <hendisantika@yahoo.co.id>
2024-05-13 09:12:54 +0700

    Add getAllEmployees in EmployeeController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springboothateoassample1/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/springboothateoassample1/controller/EmployeeController.java
@@ -1,8 +1,15 @@
 package id.my.hendisantika.springboothateoassample1.controller;
 
+import id.my.hendisantika.springboothateoassample1.model.Employee;
+import id.my.hendisantika.springboothateoassample1.model.EmployeeList;
 import id.my.hendisantika.springboothateoassample1.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Link;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +26,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private final EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public EmployeeList getAllEmployees() {
+        EmployeeList employeesList = new EmployeeList();
+
+        for (Employee employee : employeeRepository.findAll()) {
+            addLinkToEmployee(employee);
+            employeesList.getEmployees().add(employee);
+        }
+
+        // Adding self link employee collection resource
+        Link selfLink = linkTo(methodOn(EmployeeController.class).getAllEmployees()).withSelfRel();
+        employeesList.add(selfLink);
+
+        return employeesList;
+    }
 }
